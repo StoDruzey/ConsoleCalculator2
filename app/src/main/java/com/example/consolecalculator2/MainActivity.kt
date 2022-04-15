a32a45c10d530dd5f2209450b21bd4a6cd72e61d
StoDruzey <93684569+StoDruzey@users.noreply.github.com>
2022-04-15 10:29:45 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/consolecalculator2/MainActivity.kt
+++ app/src/main/java/com/example/consolecalculator2/MainActivity.kt
@@ -1,15 +1,4 @@
 package com.example.consolecalculator2
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.calculator_screen, Calculator())
-            .commit()
-    }
 }
